4abcf59fbaa19a3671173e12e21e20c5b1ad66d8
alisonmacedo <alison-macedo@hotmail.fr>
2018-11-12 20:19:09 +0100

    div class button offres, demande et deals

--- application/views/view_accueil.php
+++ application/views/view_accueil.php
@@ -34,12 +34,15 @@
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ml-auto">
                     <li class="dropdown nav-item">
-                    <a href="<?php echo base_url('controller_main/AfficherLesUsers'); ?>">
+                    <a href="<?php echo base_url('controller_main/AfficherLesUsers'); ?>" class="dropdown-toggle nav-link">
                           Nos utilisateurs
                       </a>
+                      </li>
+                      <li class="dropdown nav-item">
                       <a href="deconnexion" class="dropdown-toggle nav-link">
                           Déconnexion
                       </a>
+                      </li>
     </nav>
     
     <div class="page-header header-filter" data-parallax="true" style="background-image:url('http://wallpapere.org/wp-content/uploads/2012/02/black-and-white-city-night.png');"></div>
@@ -85,34 +88,15 @@
     	    	</div>
             </div>
         
-          <div class="tab-content tab-space">
-              
-              </div>
-
+            <div class="tab-content tab-space">
+            <div class="tab-pane active text-center gallery" id="studio">
+            **********
+  			</div>
             <div class="tab-pane text-center gallery" id="works">
-      			<div class="row">
-      				<div class="col-md-3 ml-auto">
-                      <img src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80" class="rounded">
-  					  <img src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80" class="rounded">
-  					  <img src="https://images.unsplash.com/photo-1505784045224-1247b2b29cf3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec2bdc92a9687b6af5089b335691830e&auto=format&fit=crop&w=750&q=80" class="rounded">  					</div>
-      				<div class="col-md-3 mr-auto">
-                      <img src="https://images.unsplash.com/photo-1504346466600-714572c4b726?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6754ded479383b7e3144de310fa88277&auto=format&fit=crop&w=750&q=80" class="rounded">
-                      <img src="https://images.unsplash.com/photo-1494028698538-2cd52a400b17?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83bf0e71786922a80c420c17b664a1f5&auto=format&fit=crop&w=334&q=80" class="rounded">
-      				</div>
-      			</div>
+      			**
   			</div>
             <div class="tab-pane text-center gallery" id="favorite">
-      			<div class="row">
-      				<div class="col-md-3 ml-auto">
-      				  <img src="https://images.unsplash.com/photo-1504346466600-714572c4b726?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6754ded479383b7e3144de310fa88277&auto=format&fit=crop&w=750&q=80" class="rounded">
-                      <img src="https://images.unsplash.com/photo-1494028698538-2cd52a400b17?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83bf0e71786922a80c420c17b664a1f5&auto=format&fit=crop&w=334&q=80" class="rounded">
-      				</div>
-      				<div class="col-md-3 mr-auto">
-      				  <img src="https://images.unsplash.com/photo-1505784045224-1247b2b29cf3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec2bdc92a9687b6af5089b335691830e&auto=format&fit=crop&w=750&q=80" class="rounded">  					
-      				  <img src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80" class="rounded">
-  					  <img src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80" class="rounded">
-      			    </div>
-      			</div>
+      			***
       		</div>
           </div>
 
