d813577b16139563f3cc9850a6c9a1ad46a0df75
alisonmacedo <alison-macedo@hotmail.fr>
2018-11-12 23:23:08 +0100

    début du model offre (affichage des offres de l'utilisateur co)

--- application/models/model_offre.php
+++ application/models/model_offre.php
@@ -1,8 +1,7 @@
 <?php
 class model_offre extends CI_Model{
-
+    function getOffres($idLogin){
+        $sql = $this->db->query("select idOffre, descriptionOffre, dateOffre, idService from offre inner join user on offre.idUser=user.idUser where login=".$idLogin);
+    }
 }
-
-
-
 ?>
