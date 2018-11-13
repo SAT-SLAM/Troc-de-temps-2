7185be7ce94a7ecc1869a92d48ccbea7066b302d
AUBOIRON <auboiron_sacha@yahoo.fr>
2018-11-13 14:42:11 +0100

    Changement ma fonction css

--- application/views/view_userSacha.php
+++ application/views/view_userSacha.php
@@ -5,56 +5,80 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Tous les utilisateurs</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../CSS/accueil.css">
     <script type="text/javascript" src="../JQuery/jquery-3.1.1.js"></script>
     <script type="text/javascript" src="../JS/mesFonctions.js"></script>
-    <link href="<?php echo base_url(); ?>CSS/agency.min.css" rel="stylesheet">
+	<script src="../JS/jquery.min.js"></script>
+	<script src="../JS/jquery.easing.1.3.js"></script>
+	<script src="../JS/bootstrap.min.js"></script>
+	<script src="../JS/jquery.waypoints.min.js"></script>
+	<script src="../JS/owl.carousel.min.js"></script>
+	<script src="../JS/jquery.magnific-popup.min.js"></script>
+	<script src="../JS/magnific-popup-options.js"></script>
+	<script src="../JS/main.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>CSS/agency.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>CSS/animate.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>CSS/icomoon.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>CSS/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>CSS/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>CSS/owl.theme.default.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>CSS/magnific-popup.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>CSS/style.css">
 </head>
 <body>
-<section class="bg-light" id="team">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2 class="section-heading text-uppercase">Nos incroyables utilisateurs !</h2>
-            <h3 class="section-subheading text-muted">                                       </h3>
-          </div>
-        </div>
+<div id="colorlib-page">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="colorlib-navbar-brand">
+							<a class="colorlib-logo" >Nos incroyables utilisateurs</a>
+						</div>
+					
+					</div>
+				</div>
+			</div>
+        </header>
         <div class="row">
         <?php
               foreach($lesUsers as $unUser){
 
               ?>
-          <div class="col-sm-4">
-            <div class="team-member1">
-              <img class="mx-auto rounded-circle" src="<?php echo $unUser->photoUser?>">
-              <div class="team-member">
-                <h4>
-                  <?php
-                    echo "<option value='" .$unUser->idUser. "'>".$unUser->nomUser."</option>";
-                  ?>
-                  <br>
-                    </div>
-                </h4>
-            </div>
-                  <div class="team-member">
-                  <?php
-                       if ($unUser->sexe == 0){
-                         echo  '<img src="https://nsa39.casimages.com/img/2018/10/17/mini_181017020836938365.png" />';
+		
+		<div id="colorlib-blog">
+			<div class="container">
+				<div class="row text-center">
+					
+                </div>
+                <div class="container">
+					<div class="col-md-4">
+						<div class="article animate-box">
+							<a class="blog-img">
+								<img class="img-responsive" src="<?php echo $unUser->photoUser?>">
+								<div class="overlay"></div>
+								
+							</a>
+							<div class="desc">
+								<span class="meta"> <?php echo "<option value='" .$unUser->idUser. "'>".$unUser->nomUser."</option>"; ?></span>
+								<h2><?php
+                                     if ($unUser->sexe == 0){
+                                         echo  '<img src="https://nsa39.casimages.com/img/2018/10/17/mini_181017020836938365.png" />';
                         
-                       }
-                        else
-                          echo '<img src="https://nsa39.casimages.com/img/2018/10/17/mini_181017020836876248.png" />';
+                                     }
+                                     else
+                                         echo '<img src="https://nsa39.casimages.com/img/2018/10/17/mini_181017020836876248.png" />';
 
-                  ?>
-                  </div>
-                  <p class="text-muted">
-              </p>
-          </div>
-          <?php
+                                    ?>
+                                </h2>
+							
+							</div>
+						</div>
+					</div>
+                </div>
+                <?php
               }
           ?>
-        </div>
-      </div>
-    </section>
+		</div>
+    </div>
+</div>
 </body>
 </html>
